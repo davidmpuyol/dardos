f77c9532ce8d0eed6707199110307428315957bc
Marcocuma <46998207+Marcocuma@users.noreply.github.com>
2020-05-15 13:06:25 +0200

    Lista de torneos con filtro

---
+++ src/components/cardTorneo.vue
@@ -0,0 +1,38 @@
+<template lang="html">
+
+  <div class="card">
+    <img class="card-img-top" src="../assets/bg1.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">{{torneo.nombre}}</h5>
+      <md-button class="md-raised b-0" >Ver detalles</md-button>
+    </div>
+  </div>
+
+</template>
+
+<script lang="js">
+
+  export default  {
+    name: 'card-torneo',
+    props: ["torneo"],
+    mounted () {
+
+    },
+    data () {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+
+
+</script>
+
+<style scoped lang="css">
+</style>
--- src/components/torneos.vue
+++ src/components/torneos.vue
@@ -3,66 +3,64 @@
   <section class="torneos">
     <section v-if="this.user.tipo_usuario >= 2">
 
-      <div class="contenedorCarousel">
-        <div class="carouselTorneosUser">
-          <div class="torneoC">
-            <md-button class="md-raised b-0" >boton ejemplo</md-button>
-          </div>
-          <div class="torneoC"></div>
-          <div class="torneoC"></div>
-          <div class="torneoC"></div>
-          <div class="torneoC"></div>
-          <div class="torneoC"></div>
-          <div class="torneoC"></div>
+    </section>
+    <section id="buscador" class="m-3 m-md-4 m-lg-5">
+      <div class="input-group">
+        <select id="selectTipo" v-model="fTipo" class="">
+          <option value="">Todos</option>
+          <option value=1>Oficial</option>
+          <option value=2>Comunidad</option>
+        </select>
+        <div class="input-group-append" id="inputBuscador">
+          <input class="input-group-text w-100" v-model="fNombre" type="text"/>
         </div>
       </div>
+    </section>
+    <section class="row">
+      <cardTorneo class="cardTorneo col-lg-2 col-sm-4 col-12 p-0" v-for="torneo in this.listaTorneos" :torneo="torneo"></cardTorneo>
     </section>
   </section>
 
 </template>
 
 <script lang="js">
-
+  import cardTorneo from "./cardTorneo.vue"
   export default  {
     name: 'torneos',
     props: ['conexion','user'],
+    components: {
+      cardTorneo
+    }, 
     mounted () {
-       $('.carouselTorneosUser').slick({
-          arrows: true,
-          centerPadding: '60px',
-          slidesToShow: 3,
-          responsive: [
-          {
-          breakpoint: 768,
-          settings: {
-              arrows: false,
-              centerMode: true,
-              centerPadding: '40px',
-              slidesToShow: 1
-          }
-          },
-          {
-          breakpoint: 480,
-          settings: {
-              arrows: false,
-              centerMode: true,
-              centerPadding: '40px',
-              slidesToShow: 1
-          }
-          }
-      ]
-      });
+      //Los torneos tipo 1 son los torneos oficiales y los torneo tipo 2 son los de la comunidad
+      this.conexion.on('resultadoTorneos',(resultado)=>{
+        this.torneos = resultado
+        console.log(this.torneos)
+      })
+      this.conexion.emit("getTorneos")
     },
     data () {
       return {
-
+        torneos: [],
+        fNombre: "",
+        fTipo:"",
       }
     },
     methods: {
 
     },
     computed: {
-
+      listaTorneos:function(){
+        return this.torneos.filter((torneo)=>{
+          if(this.fTipo != ""){
+            if(torneo.tipo==this.fTipo&&torneo.nombre.includes(this.fNombre))
+              return torneo
+          }else{
+            if(torneo.nombre.includes(this.fNombre))
+              return torneo
+          }
+        })
+      }
     }
 }
 
@@ -72,5 +70,20 @@
 <style scoped lang="css">
   .torneos {
 
+  }
+  .cardTorneo{
+    box-shadow: 0 0.5em 1em -0.125em rgba(10,10,10,.1), 0 0 0 1px rgba(10,10,10,.02);
+  }
+  #inputBuscador{
+    width: 80%;
+  }
+  #inputBuscador input{
+    border-top-right-radius: 0.7em;
+    border-bottom-right-radius: 0.7em;
+  }
+  #selectTipo{
+    width: 20%;
+    border-top-left-radius: 0.7em;
+    border-bottom-left-radius: 0.7em;;
   }
 </style>
